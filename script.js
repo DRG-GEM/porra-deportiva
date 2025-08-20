ca6257e9fb32a433ab4eb52429e79f554ce875fa
DRG-GEM <donraulvp@gmail.com>
2025-08-20 11:57:01 +0200

    Añade la configuración de Firebase

---
+++ script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPKvV9IXP5Ptayct4t3DxCd36yfPiHf4U",
+  authDomain: "porra-deportiva.firebaseapp.com",
+  projectId: "porra-deportiva",
+  storageBucket: "porra-deportiva.firebasestorage.app",
+  messagingSenderId: "321093458886",
+  appId: "1:321093458886:web:ed44b09be50520480630c7"
+};
